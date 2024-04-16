1398fcfa838050f649a689523af0a7059b96cdc4
Witold Hajkowicz <witold.hajkowicz@student.put.poznan.pl>
2024-04-16 19:08:00 +0200

    update greedy

--- really_greedy/main.py
+++ really_greedy/main.py
@@ -13,24 +13,25 @@ def greedy(sequences, full_sequence_length):
     sequence = random.choice(sequences)
     sequences.remove(sequence)
     sequences_used = 1
+    coverage = 0
     while len(sequence) < full_sequence_length:
-        print(len(sequence))
+        # print(len(sequence))
         next_sequence, overlaping_next = find_best_next_sequence(sequence, sequences)
         prev_sequence, overlaping_prev = find_best_prev_sequence(sequence, sequences)
-        if overlaping_next > 0:
+        if overlaping_next > 0 and overlaping_next > overlaping_prev:
             sequence += next_sequence[overlaping_next:]
             sequences.remove(next_sequence)
             sequences_used += 1
-        if overlaping_prev > 0:
+        if overlaping_prev > 0 and overlaping_prev >= overlaping_next:
             sequence = prev_sequence[:overlaping_prev] + sequence
             sequences.remove(prev_sequence)
             sequences_used += 1
         if not overlaping_prev and not overlaping_next:
-            return sequence
-    print(len(sequence))
-    coverage = sequences_used * 10 / len(sequence)
-    print("Coverage: ", coverage)
-    return sequence
+            return sequence, coverage
+    # print(len(sequence))
+    coverage = sequences_used / len(sequence)
+    # print("Coverage: ", coverage)
+    return sequence, coverage
 
 
 def compare_next_sequences(sequence, next_sequence, l=10):
@@ -65,8 +66,20 @@ def find_best_prev_sequence(sequence, sequences):
 
 if __name__ == "__main__":
     data = read_sequences_from_file("sequences_negative.txt")
-    print(data)
-    print(compare_next_sequences("abc", "bcd"))
-    print(find_best_next_sequence("abc", ["bcd", "cde", "def", "efg", "fgh", "ghi", "hij", "ijk", "jkl", "klm", "lmn", "mno", "nop", "opq", "pqr", "qrs", "rst", "stu", "tuv", "uvw", "vwx", "wxy", "xyz", "yza", "zab"]))
-    print(find_best_prev_sequence("abc", ["bcd", "cde", "def", "efg", "fgh", "ghi", "hij", "ijk", "jkl", "klm", "lmn", "mno", "nop", "opq", "pqr", "qrs", "rst", "stu", "tuv", "uvw", "vwx", "wxy", "xyz", "yza", "zab"]))
-    print(greedy(data, 209))
+    # print(data)
+    # print(compare_next_sequences("abc", "bcd"))
+    # print(find_best_next_sequence("abc", ["bcd", "cde", "def", "efg", "fgh", "ghi", "hij", "ijk", "jkl", "klm", "lmn", "mno", "nop", "opq", "pqr", "qrs", "rst", "stu", "tuv", "uvw", "vwx", "wxy", "xyz", "yza", "zab"]))
+    # print(find_best_prev_sequence("abc", ["bcd", "cde", "def", "efg", "fgh", "ghi", "hij", "ijk", "jkl", "klm", "lmn", "mno", "nop", "opq", "pqr", "qrs", "rst", "stu", "tuv", "uvw", "vwx", "wxy", "xyz", "yza", "zab"]))
+    best_seq = ''
+    best_coverage = 0
+    for i in range(1000):
+        result, coverage = greedy(data.copy(), 209)
+        # print(result)
+        # print(coverage)
+        if best_coverage < coverage:
+            best_seq = result
+            best_coverage = coverage
+        if coverage > 0.8:
+            print(coverage)
+    print(best_coverage)
+    print(best_seq)
